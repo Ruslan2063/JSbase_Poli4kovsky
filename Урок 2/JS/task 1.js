86f24850a6ea3df6c3ae687fab1bb11683c21962
Ruslan <ruslansklar05@gmail.com>
2021-03-17 21:51:30 +0200

    HW 2

--- Урок 2/JS/task 1.js
+++ Урок 2/JS/task 1.js
@@ -1,10 +1,10 @@
-var a = prompt("Введіть перше число:");
-var b = prompt("Введіть друге число:");
-var c = prompt("Введіть третє число:");
+let a = +prompt("Введіть перше число:");
+let b = +prompt("Введіть друге число:");
+let c = +prompt("Введіть третє число:");
 
 if (a !== b && b !== c && a !== c) {
-	var sum = a + b + c;
-	var sum = sum / 3;
+	let sum = a + b + c;
+	sum = sum / 3;
 	document.write("Середнє арифметичне:" + sum);
 }
 // else {
