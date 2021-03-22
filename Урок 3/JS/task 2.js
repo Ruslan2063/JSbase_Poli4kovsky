8fc49407a1ba21289cc96863a7c3da33e95e860e
Ruslan <ruslansklar05@gmail.com>
2021-03-22 21:55:25 +0200

    HW 3

--- Урок 3/JS/task 2.js
+++ Урок 3/JS/task 2.js
@@ -1,16 +1,16 @@
 let odd = ["odd", "odd", "odd"];
 let even = ["even", "even", "even"];
 
-let size = prompt("Enter size masiv: ");
+let size = +prompt("Enter size masiv: ");
 let masiv = [];
 
 for (let i = 0; i < size; i++) {
 	if (i % 2 == 0) {
-		masiv[i] = even;
+		masiv[i] = ["even", "even", "even"];
 	}
 
 	else {
-		masiv[i] = odd;
+		masiv[i] = ["odd", "odd", "odd"];
 	}
 }
 console.log(masiv);
--- Урок 3/JS/task 3.js
+++ Урок 3/JS/task 3.js
@@ -1,4 +1,16 @@
 let fructs = ["Apple", "Orange", "Limon", "Strawberry", "Peach"];
+console.log("Our fructs: " + fructs);
 let show = prompt("Enter fructs: " + fructs);
-console.log("Our fructs: " + show);
-if()
+
+let plusEl = prompt("Which one fructs: ");
+for (let i = 0; i <= fructs.length; i++) {
+	if (fructs[i] == show) {
+		fructs.splice(i + 2, 1, plusEl);
+		break;
+	}
+	if (i == fructs.length) {
+		fructs.push(plusEl);
+		break;
+	}
+}
+console.log("Last masiv:" + fructs);
